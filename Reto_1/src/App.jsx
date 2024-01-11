971ca9719b8e8bccd8d8db199e86a93881fed6fb
lel20 <leonardoparedes.v.r@gmail.com>
2024-01-11 20:23:19 +0100

    Reto terminado

--- Reto_1/src/App.jsx
+++ Reto_1/src/App.jsx
@@ -1,23 +1,42 @@
+import './App.css';
 import { useEffect } from "react"
 import { useState, } from "react"
-const factRandom= 'https://catfact.ninja/fact'
-// const imageRandom=`https://cataas.com/cat/says/${fristword}`
+const factRandom = 'https://catfact.ninja/fact'
 export function App() {
-    const [randoGatito, setRandomGatitos]= useState(null)
-    const obternerGatitos= ()=>{
-        const obtenerDatosGatos= fetch(factRandom)
-        .then((res)=>res.json())
-        .then(datos=>{
-            const {fact}= datos
-            setRandomGatitos(fact)
-            const fristword= fact.split(' ', 1).join(' ')
-            console.log(fristword)
-        })
-    }
-    useEffect(()=>{
-        obternerGatitos()
-    },[])
-    return (
-        <div>{randoGatito}</div>
-    )
+  //se crea un estado para guardar los datos de la api de gatitos
+  const [randonGatito, setRandomGatitos] = useState('')
+  //se crea un estado para guardar el valor de la primera palabra de la api de gatitos
+  const [firstword, getFirstWord] = useState('')
+
+  //se crea una funcion para obtener los datos de la api de gatitos
+  const obternerGatitos = () => {
+    fetch(factRandom)
+      .then((res) => res.json())
+      .then((datos) => setRandomGatitos(datos.fact))
+  }
+  //se crea un useEffect para que se ejecute la funcion de obtener los datos de la api de gatitos. Ca
+  useEffect(() => {
+    //se ejecuta la funcion de obtener los datos de la api de gatitos
+    obternerGatitos()
+    // este useEffect se ejecuta cuando se carga la pagina
+  }, [])
+//se rea un segundo useEffect para que se ejecute la función de obtener la primera palabra de la api de gatitos
+  useEffect(() => {
+    //si no hay datos dentro del estado de randonGatito no se ejecuta la función
+    if (!randonGatito) return
+    //si hay datos dentro del estado de randonGatito se obtiene la primera palabra de la api de gatitos mediante el metodo split
+    const palabra = randonGatito.split(" ", 1).join(" ")
+    //se crea una constante para guardar la url de las imagenes de gatitos y esta se le pasa la constante de palabra para que se muestre en la imagen
+    const url = `https://cataas.com/cat/says/${palabra}?fontSize=50&fontColor=red`
+    
+    getFirstWord(url)
+    // este useEffect se ejecuta cuando cambia el estado de randonGatito
+  },[randonGatito])
+
+  return (
+    <div>
+      <p>{randonGatito}</p>
+      <img src={firstword} alt="" />
+    </div>
+  )
 }
